afc81e9bb6edb0124571cf6c9e918560dc47bd54
david <daviddias.aeu@gmail.com>
2024-06-12 19:07:07 +0100

    cont

--- exercicios/FichasJava/src/FichaPratica02/Exercicio09.java
+++ exercicios/FichasJava/src/FichaPratica02/Exercicio09.java
@@ -20,6 +20,15 @@ public class Exercicio09 {
         System.out.print("Terceiro número:");
         int num3 = input.nextInt();
 
+        if (num1<num2 && num1<num3){
+            System.out.println("O número menor é: " + num1);
+        } else if (num2<num1 && num2<num3){
+            System.out.println("O número menor é: " + num2);
+        } else{
+            System.out.println("O número menor é: " + num3);
+
+        }
+
 
     }
 }
