14e4f644abd7662a7cd60f8000238ccd9802348e
david <daviddias.aeu@gmail.com>
2024-06-25 12:50:54 +0100

    cont

---
+++ exercicios/FichasJava/src/FichaPratica04/Exercicio04.java
@@ -0,0 +1,30 @@
+package FichaPratica04;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+
+        boolean numeroPrimo = true;
+
+        System.out.print("Introduza um valor inteiro para sabermos se é um número primo:");
+        int num1 = input.nextInt();
+
+
+        if (num1 % 2 == 0){
+            numeroPrimo = false;
+        }
+
+
+            if (num1 / 1 == num1 && num1 / num1 == 1) {
+                System.out.println("É primo!");
+            } else {
+                System.out.println("Não é primo.");
+            }
+
+    }
+}
