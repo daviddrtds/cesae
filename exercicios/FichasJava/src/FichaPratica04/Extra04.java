4244eebc3e5d55584d9db323f165cc44f97f71f4
david <daviddias.aeu@gmail.com>
2024-06-27 16:22:44 +0100

    cont

---
+++ exercicios/FichasJava/src/FichaPratica04/Extra04.java
@@ -0,0 +1,15 @@
+package FichaPratica04;
+
+public class Extra04 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+            for (int y = 1; y <= 10; y++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
