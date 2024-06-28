2905944682b3de350bcaf469028d59e09d8652ed
david <daviddias.aeu@gmail.com>
2024-06-28 10:37:25 +0100

    cont

--- exercicios/FichasJava/src/FichaPratica05/Exercicio03.java
+++ exercicios/FichasJava/src/FichaPratica05/Exercicio03.java
@@ -1,8 +1,30 @@
 package FichaPratica05;
 
+import java.util.Scanner;
+
 public class Exercicio03 {
 
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[] novoArray = new int[3];
+        int anterior = 0;
+        int valorMaior = 0;
+
+
+        for (int i = 0; i < novoArray.length; i++) {
+            System.out.print("insira um número inteiro: ");
+            novoArray[i] = input.nextInt();
+
+            if (novoArray[i] > anterior) {
+                valorMaior = novoArray[i];
+                anterior = novoArray[i];
+            }
+
+        }
+
+        System.out.println(valorMaior);
 
     }
 }
---
+++ exercicios/FichasJava/src/FichaPratica05/Exercicio04.java
@@ -0,0 +1,7 @@
+package FichaPratica05;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+
+    }
+}
