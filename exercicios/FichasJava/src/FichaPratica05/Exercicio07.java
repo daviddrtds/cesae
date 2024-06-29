d83c99646800a66b65a123e7cc8b44bb8e30abc4
david <daviddias.aeu@gmail.com>
2024-06-29 10:17:39 +0100

    cont

--- exercicios/FichasJava/src/FichaPratica05/Exercicio07.java
+++ exercicios/FichasJava/src/FichaPratica05/Exercicio07.java
@@ -10,6 +10,7 @@ public class Exercicio07 {
 
         int[] novoArray = new int[3];
         int parMaior;
+        boolean encontrouPar = false;
 
 
         for (int i = 0; i < novoArray.length; i++) {
@@ -17,13 +18,30 @@ public class Exercicio07 {
             novoArray[i] = input.nextInt();
         }
 
+
         parMaior = novoArray[0];
 
-        for (int i = 0; i < novoArray.length; i++) {
+
+        if (parMaior % 2 == 0) {    // verifica logo se index 0 é par
+            encontrouPar = true;
+        }
+
+
+        for (int i = 1; i < novoArray.length; i++) {     // proxima verificação começa no index 1
             if (novoArray[i] % 2 == 0) {
 
-
+                if (novoArray[i] > parMaior || !encontrouPar) {
+                    parMaior = novoArray[i];
+                    encontrouPar = true;
+                }
             }
+        }
+
+
+        if (encontrouPar) {
+            System.out.println("O maior número par inserido é: " + parMaior);
+        } else {
+            System.out.println("Nenhum número par inserido");
         }
 
 
