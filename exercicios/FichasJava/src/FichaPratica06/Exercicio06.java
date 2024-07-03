e816369bb0bf55fad0865479fd26bad21473ccdc
david <daviddias.aeu@gmail.com>
2024-07-03 18:47:36 +0100

    cont

--- exercicios/FichasJava/src/FichaPratica06/Exercicio06.java
+++ exercicios/FichasJava/src/FichaPratica06/Exercicio06.java
@@ -2,6 +2,8 @@ package FichaPratica06;
 
 import java.util.Scanner;
 
+import static FichaPratica06.Exercicio05.*;
+
 public class Exercicio06 {
 
 
@@ -9,8 +11,51 @@ public class Exercicio06 {
 
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Insira o tamanho do array: ");
-        String animal = input.next().toLowerCase();
+        int arraySize;
+
+        do {
+            System.out.print("Insira un número maior do que 0 para definir o tamanho do array: ");
+            arraySize = input.nextInt();
+
+        } while (arraySize <= 0);
+
+
+        int[] newArr = new int[arraySize];
+
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.print("Introduza o número para o index [" + i + "]: ");
+            newArr[i] = input.nextInt();
+        }
+
+        int opcaoMenu;
+
+        do {
+            System.out.println("Selecione uma opção:");
+            System.out.println("7. Ver o maior número do array");
+            System.out.println("8. Ver o menor número do array");
+            System.out.println("9. Ver se o array é crescente");
+            opcaoMenu = input.nextInt();
+
+            switch (opcaoMenu) {
+                case 7:
+                    System.out.println("O maior número do array é " + maiorArr(newArr));
+                    break;
+
+                case 8:
+                    System.out.println("O menor número do array é " + menorArr(newArr));
+                    break;
+
+                case 9:
+                    System.out.println("O array é crescente? " + isCrescentArr(newArr));
+                    break;
+
+                default:
+                    System.out.println("Opção de menu inválida");
+                    break;
+            }
+
+
+        } while (opcaoMenu <= 9 && opcaoMenu >= 7);
 
 
     }
