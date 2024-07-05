2c6ad872d4d2ee66895d19b3ee7efe669026f0b6
david <daviddias.aeu@gmail.com>
2024-07-05 19:48:31 +0100

    cont

--- exercicios/FichasJava/src/FichaPratica07/Exercicio09.java
+++ exercicios/FichasJava/src/FichaPratica07/Exercicio09.java
@@ -23,35 +23,46 @@ public class Exercicio09 {
         }
 
         Scanner scanner2 = new Scanner(fileTxt);
-
+        String cabecalho = scanner2.nextLine();
+        String cabecalho2= scanner2.nextLine();
+        String[] arr2 = cabecalho2.split(",");
         String[][] matrix1 = new String[numLinhas][arr1.length];
 
-        while (scanner.hasNextLine()) {
-            primeiraLinha = scanner.nextLine();
-            arr1 = primeiraLinha.split(",");
-            matrix1 = new String[numLinhas][arr1.length];
-            for (int i = 0; i < matrix1.length; i++) {
-                for (int j = 0; j < matrix1[0].length; j++) {
-                    matrix1[i][j] = arr1[j];
-                }
+//        while (scanner2.hasNextLine()) {
+//            primeiraLinha = scanner2.nextLine();
+//            arr1 = primeiraLinha.split(",");
+//
+//            for (int i = 0; i < matrix1.length; i++) {
+//                for (int j = 0; j < matrix1[0].length; j++) {
+//                    matrix1[i][j] = arr1[j];
+//                }
+//
+//                primeiraLinha = scanner2.nextLine();
+//                arr1 = primeiraLinha.split(",");
+//            }
+//        }
+
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[0].length; j++) {
+                matrix1[i][j] = arr2[j];
             }
+            cabecalho2 = scanner2.nextLine();
+            arr2 = cabecalho2.split(",");
         }
 
         return matrix1;
-
-
     }
 
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        String[][] matrizTotal = tabelaMusica("");
+        String[][] matrizTotal = tabelaMusica("FicheirosFicha07/exercicio_09.csv");
 
         for (int i = 0; i < matrizTotal.length; i++) {
             for (int j = 0; j < matrizTotal[0].length; j++) {
                 System.out.print(matrizTotal[i][j] + " ");
             }
-            System.out.println(" ");
+            System.out.println();
         }
     }
 }
