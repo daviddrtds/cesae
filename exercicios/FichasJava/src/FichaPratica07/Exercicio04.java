b939d6023485fd93dd5ff36bcbb11146ec936265
david <daviddias.aeu@gmail.com>
2024-07-05 12:49:47 +0100

    cont

---
+++ exercicios/FichasJava/src/FichaPratica07/Exercicio04.java
@@ -0,0 +1,31 @@
+package FichaPratica07;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Exercicio04 {
+
+    public static void lerExcel(String path) throws FileNotFoundException {
+
+        File fileOrigem = new File(path);
+        Scanner scannFicheiro = new Scanner(fileOrigem);
+
+        while (scannFicheiro.hasNext()) {
+            String novaLinha = scannFicheiro.nextLine();
+            String[] arr = novaLinha.split(";");
+            for (int i = 0; i < arr.length; i++) {
+
+                arr[i] = arr[i].trim();
+                arr[i] = " " + arr[i] + " ";
+
+                System.out.print(arr[i]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        lerExcel("FicheirosFicha07/exercicio_04.csv");
+    }
+}
